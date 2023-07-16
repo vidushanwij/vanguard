37aaeb1d8b76858a4a482d8139d287ab3758d43a
Vidushan Wijesinghe <wijesinghe.vidu@gmail.com>
2023-07-16 13:22:40 +0530

    Initial Commit

---
+++ src/test/java/com/vanguard/warehousemanagement/WarehousemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.warehousemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
